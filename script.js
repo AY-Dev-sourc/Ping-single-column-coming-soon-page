6b35387f192d7c052a983bea60e9e8f5d57e0ddc
Ayoub dev <khazzania57@gmail.com>
2025-08-11 15:51:50 +0100

    Ping single column coming soon page

---
+++ script.js
@@ -0,0 +1,40 @@
+const form = document.getElementById('form');
+const emailInput = document.getElementById('emailInput');
+
+
+function EmailValidation(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function SetError(Input,msg) {
+    Input.classList.add('error');
+    Input.classList.remove('success');
+
+    const inputField = Input.parentElement;
+    const message = inputField.querySelector('.message');
+
+    message.innerHTML = msg;
+}
+
+function SetSuccess(Input) {
+    Input.classList.add('success');
+    Input.classList.remove('error');
+    const inputField = Input.parentElement;
+    const message = inputField.querySelector('.message');
+
+    message.innerHTML = '';
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    let value = emailInput.value;
+    if (value === '') {
+        SetError(emailInput,'Please provide valid email address');
+    } else if(!EmailValidation(value)){
+        SetError(emailInput,`The Email Is Faulse`);
+    }
+    else{
+        SetSuccess(emailInput);
+    }
+});
